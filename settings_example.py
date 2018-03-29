c889713caec3bcc1581768239b268957b69b72f5
seLain <selain@nature.ee.ncku.edu.tw>
2018-03-29 19:12:52 +0800

    missed settings example

--- settings_example.py
+++ settings_example.py
@@ -17,6 +17,12 @@ DEFAULT_SOURCE_ROOT_DIR = 'source_repos'
 # The value can only be one of ['GitPython', 'PyGithub']
 DEFAULT_COMMIT_TOOL = 'GitPython'
 
+# WaitStrategy selection. The value must be one of the concrete wait strategies in wait_strategy.py
+WAIT_STRATEGY = 'RandomWorkHourStrategy'
+
 # Random commit interval
 RANDOM_MIN_MINUTES = 60
 RANDOM_MAX_MINUTES = 120
+
+# Bot working hours
+WORK_HOURS = [9, 18] # from 9:00AM~6:00PM
